5cdce8472ae7a21a56d285d754cfa8001b997663
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-10-29 17:04:53 +0000

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import WelcomeDashboard from '@/components/Custom/welcome_dashboard'
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+       <h1>we good</h1>
+      </div>
+  )
+}
+
+export default Home
